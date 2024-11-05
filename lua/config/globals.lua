836c914b45e1aaa491f665691a663c6c31c0e8c8
Shumy <razvansuhan@yahoo.com>
2024-11-05 15:51:15 +0200

    Added Harpoon

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,4 +1,3 @@
-vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.api.nvim_set_keymap("n", "<Space>", ":WhichKey<cr>", {silent = true, noremap = true})
