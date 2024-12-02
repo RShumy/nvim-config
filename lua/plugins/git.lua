bce708fbcae60e4a8d0343815080fe2f41ff2e12
RShumy <razvansuhan@yahoo.com>
2024-12-02 23:59:35 +0200

    Add obsidian for note taking and refining git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -16,11 +16,16 @@ return {
 			-- Set a vim motion to <Space> + g + <Shift>A to all files changed to the staging area
 			vim.keymap.set("n", "<leader>gA", ":Git add .<cr>", { desc = "[G]it Add [A]ll" })
 			-- Set a vim motion to <Space> + g + a to add the current file and changes to the staging area
-			vim.keymap.set("n", "<leader>ga", ":Git add", { desc = "[G]it [A]dd" })
+			vim.keymap.set("n", "<leader>ga", ":Git add ", { desc = "[G]it [A]dd" })
 			-- Set a vim motion to <Space> + g + c to commit the current chages
-			vim.keymap.set("n", "<leader>gc", ":Git commit", { desc = "[G]it [C]ommit" })
+			vim.keymap.set("n", "<leader>gc", ":Git commit -m ", { desc = "[G]it [C]ommit" })
 			-- Set a vim motion to <Space> + g + p to push the commited changes to the remote repository
 			vim.keymap.set("n", "<leader>gp", ":Git push<cr>", { desc = "[G]it [P]ush" })
+			-- Set a vim motion to <Space> + g + P to force push to origin the commited changes to the remote repository
+			vim.keymap.set("n", "<leader>gP", ":Git push origin --force<cr>", { desc = "[G]it [P]ush" })
+
+			-- Set a vim motion to <Space> + g + r to rebase interactively
+			vim.keymap.set("n", "<leader>gr", ":Git rebase -i HEAD~", { desc = "[G]it [P]ush" })
 		end,
     }
 }
